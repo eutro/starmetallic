c3a982f2e7d14b64b0d6d82789198dc2e06b0599
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-10-18 12:22:10 +0100

    It kinda runs.

--- src/main/clojure/eutros/starmetallic/compilerhack/clinitfilter.clj
+++ src/main/clojure/eutros/starmetallic/compilerhack/clinitfilter.clj
@@ -1,70 +1,73 @@
-(ns eutros.starmetallic.compilerhack.clinitfilter
-  (:require [instrumentation.core :refer [INSTRUMENTATION]]
-            [instrumentation.dynamic :refer [attach]])
-  (:import (java.lang.instrument Instrumentation ClassFileTransformer)
-           (org.objectweb.asm ClassReader ClassWriter Opcodes)
-           (org.objectweb.asm.tree ClassNode MethodNode MethodInsnNode)))
+(ns eutros.starmetallic.compilerhack.clinitfilter)
 
-(attach)
+(when *compile-files*
+  (require '[instrumentation.core :refer [INSTRUMENTATION]]
+           '[instrumentation.dynamic :refer [attach]])
+  (import (java.lang.instrument Instrumentation ClassFileTransformer)
+          (org.objectweb.asm ClassReader ClassWriter Opcodes)
+          (org.objectweb.asm.tree ClassNode MethodNode MethodInsnNode)))
 
-(defn transform
-  [class func]
-  (let [transformer
-        (reify ClassFileTransformer
-          (transform [_ _ _ cbr _ buf]
-            (try
-              (when (= class cbr)
-                (let [cn (ClassNode.)]
-                  (.accept (ClassReader. buf) cn 0)
-                  (func cn)
-                  (.toByteArray
-                    (doto (ClassWriter. ClassWriter/COMPUTE_FRAMES)
-                      (->> (.accept cn))))))
-              (catch Throwable t#
-                (.printStackTrace t#)))))]
-    (println "Redefining class:" class)
-    (doto ^Instrumentation INSTRUMENTATION
-      (.addTransformer transformer true)
-      (.retransformClasses (into-array [class]))
-      (.removeTransformer transformer))
-    (println "Redefined class:" class)))
+(when *compile-files*
+  (attach)
 
-(defn find-method
-  [^ClassNode node name desc]
-  (some (fn [^MethodNode node]
-          (and (= (.-name node) name)
-               (= (.-desc node) desc)
-               node))
-        (.-methods node)))
+  (defn transform
+    [class func]
+    (let [transformer
+          (reify ClassFileTransformer
+            (transform [_ _ _ cbr _ buf]
+              (try
+                (when (= class cbr)
+                  (let [cn (ClassNode.)]
+                    (.accept (ClassReader. buf) cn 0)
+                    (func cn)
+                    (.toByteArray
+                      (doto (ClassWriter. ClassWriter/COMPUTE_FRAMES)
+                        (->> (.accept cn))))))
+                (catch Throwable t#
+                  (.printStackTrace t#)))))]
+      (println "Redefining class:" class)
+      (doto ^Instrumentation INSTRUMENTATION
+        (.addTransformer transformer true)
+        (.retransformClasses (into-array [class]))
+        (.removeTransformer transformer))
+      (println "Redefined class:" class)))
 
-(defn find-insn
-  [^MethodNode node predicate]
-  (some #(and (predicate %)
-              %)
-        (.-instructions node)))
+  (defn find-method
+    [^ClassNode node name desc]
+    (some (fn [^MethodNode node]
+            (and (= (.-name node) name)
+                 (= (.-desc node) desc)
+                 node))
+          (.-methods node)))
 
-(transform Compiler
-  (fn [cn]
-    (let [method (find-method cn
-                              "maybeResolveIn"
-                              "(Lclojure/lang/Namespace;Lclojure/lang/Symbol;)Ljava/lang/Object;")
-          invokestatic (find-insn method
-                                  #(and (instance? MethodInsnNode %)
-                                        (let [^MethodInsnNode node %]
-                                          (and (= (.getOpcode node) Opcodes/INVOKESTATIC)
-                                               (= (.-owner node) "clojure/lang/RT")
-                                               (= (.-name node) "classForName")))))]
-      (set! (.-name ^MethodInsnNode invokestatic)
-            "classForNameNonLoading"))))
+  (defn find-insn
+    [^MethodNode node predicate]
+    (some #(and (predicate %)
+                %)
+          (.-instructions node)))
 
-(transform (Class/forName "clojure.core$get_proxy_class")
-  (fn [cn]
-    (let [method (find-method cn "invokeStatic" "(Lclojure/lang/ISeq;)Ljava/lang/Object;")
-          invokestatic (find-insn method
-                                  #(and (instance? MethodInsnNode %)
-                                        (let [^MethodInsnNode node %]
-                                          (and (= (.getOpcode node) Opcodes/INVOKESTATIC)
-                                               (= (.-owner node) "clojure/lang/RT")
-                                               (= (.-name node) "loadClassForName")))))]
-      (set! (.-name ^MethodInsnNode invokestatic)
-            "classForNameNonLoading"))))
+  (transform Compiler
+    (fn [cn]
+      (let [method (find-method cn
+                                "maybeResolveIn"
+                                "(Lclojure/lang/Namespace;Lclojure/lang/Symbol;)Ljava/lang/Object;")
+            invokestatic (find-insn method
+                                    #(and (instance? MethodInsnNode %)
+                                          (let [^MethodInsnNode node %]
+                                            (and (= (.getOpcode node) Opcodes/INVOKESTATIC)
+                                                 (= (.-owner node) "clojure/lang/RT")
+                                                 (= (.-name node) "classForName")))))]
+        (set! (.-name ^MethodInsnNode invokestatic)
+              "classForNameNonLoading"))))
+
+  (transform (Class/forName "clojure.core$get_proxy_class")
+    (fn [cn]
+      (let [method (find-method cn "invokeStatic" "(Lclojure/lang/ISeq;)Ljava/lang/Object;")
+            invokestatic (find-insn method
+                                    #(and (instance? MethodInsnNode %)
+                                          (let [^MethodInsnNode node %]
+                                            (and (= (.getOpcode node) Opcodes/INVOKESTATIC)
+                                                 (= (.-owner node) "clojure/lang/RT")
+                                                 (= (.-name node) "loadClassForName")))))]
+        (set! (.-name ^MethodInsnNode invokestatic)
+              "classForNameNonLoading")))))
